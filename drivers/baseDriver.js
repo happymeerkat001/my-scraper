22d9fa2b858f185e4f059ff7efb55d9c0c3c90f8
Ang Li <mr.angli@gmail.com>
2025-12-14 11:08:23 -0600

    Pin Puppeteer to Chrome for Testing executablePath

--- drivers/baseDriver.js
+++ drivers/baseDriver.js
@@ -105,6 +105,7 @@ export class BaseDriver {
         try {
           this.browser = await puppeteer.launch({
             headless: DEBUG_VISIBLE ? false : 'new',
+            executablePath: '/Users/leon/.cache/puppeteer/chrome/mac_arm-143.0.7499.42/chrome-mac-arm64/Google Chrome for Testing.app/Contents/MacOS/Google Chrome for Testing',
             args: [
               '--no-sandbox',
               '--disable-setuid-sandbox',
